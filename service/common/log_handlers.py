8073b250c29c393943ea52ed14871a27ac193fbe
candraceu <candra_ceu@yahoo.co.id>
2025-10-06 14:23:06 +0700

    Cek baris

--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 ######################################################################
 
 """
@@ -21,7 +22,6 @@ This module contains utility functions to set up logging
 consistently
 """
 import logging
-
 
 
 def init_logging(app, logger_name: str):
